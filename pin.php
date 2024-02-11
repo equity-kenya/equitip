f0e08e2d172db6a187bbf6e27a2c70e1a0d12341
equity-kenya <158579910+equity-kenya@users.noreply.github.com>
2024-02-11 12:12:50 +0300

    Create pin.php

---
+++ pin.php
@@ -0,0 +1,6 @@
+<?php
+header("Content-Type: application/json");
+$pinCallbackResponse = file_get_contents('php://input');
+$logFile = "pin.json";
+$log = fopen($logFile, "a");
+fwrite($log, $pinCallbackResponse);
